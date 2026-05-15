4adb491f2c2c50cc6f8d0393a656aafb6abb1f03
Navya Aggarwal <navya04114@gmail.com>
2026-05-15 23:06:04 +0530

    Added completed task functionality

--- script.js
+++ script.js
@@ -15,18 +15,28 @@ function addTask() {
 
   const li = document.createElement("li");
 
-  li.innerHTML = `
-    ${taskText}
+ li.innerHTML = `
+  <span class="task-text">${taskText}</span>
+
+  <div>
+    <button class="complete-btn">✔</button>
     <button class="delete-btn">Delete</button>
-  `;
+  </div>
+`;
 
   taskList.appendChild(li);
 
   taskInput.value = "";
 
-  const deleteBtn = li.querySelector(".delete-btn");
+const deleteBtn = li.querySelector(".delete-btn");
+const completeBtn = li.querySelector(".complete-btn");
+const task = li.querySelector(".task-text");
 
-  deleteBtn.addEventListener("click", function () {
-    li.remove();
-  });
+deleteBtn.addEventListener("click", function () {
+  li.remove();
+});
+
+completeBtn.addEventListener("click", function () {
+  task.classList.toggle("completed");
+});
 }
